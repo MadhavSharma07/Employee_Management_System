61064f1a78c63adafb535000a94021f824b182ce
MadhavSharma07 <122592334+MadhavSharma07@users.noreply.github.com>
2025-06-22 15:44:08 +0530

    importing the react and usestate hook

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,11 @@
-
+import React, { useState } from 'react';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <h1 className="text-3xl text-teal-500">Welcome to My project</h1>
-  )
+  );
 }
 
-export default App
+export default App;
